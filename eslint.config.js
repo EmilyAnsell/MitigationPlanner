9087a3986907f8dbb83c1d48a521f94ab4204a61
Emily Ansell <emily.a.ansell@gmail.com>
2026-06-10 16:12:47 -0400

    Added some react-specific rules for eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,16 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import react from "eslint-plugin-react";
 
 export default [
+  {
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:react/jsx-runtime",
+    ],
+  },
   { ignores: ["dist"] },
   {
     files: ["**/*.{js,jsx}"],
@@ -19,10 +27,19 @@ export default [
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      react: react,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      ...react.configs.flat["jsx-runtime"].rules,
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
