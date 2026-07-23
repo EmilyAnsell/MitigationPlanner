eac8361a161d02c61ab73cb348a8e2425fa80077
Emily Ansell <emily.a.ansell@gmail.com>
2026-07-23 12:22:25 -0400

    tiny polish on div structure

--- src/components/dialog/Dialog.jsx
+++ src/components/dialog/Dialog.jsx
@@ -1,26 +1,20 @@
 export default function Dialog({ isDialogOpen, onCloseDialog }) {
-  return (
-    <>
-      {isDialogOpen ? (
-        <>
-          <div
-            className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
-            onClick={onCloseDialog}
-          >
-            <div
-              className="fixed p-6 bg-gray-800 rounded-lg shadow-xl "
-              onClick={(e) => e.stopPropagation()}
-            >
-              <button
-                className="px-3 py-1 bg-blue-600 rounded hover:bg-blue-700"
-                onClick={onCloseDialog}
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        </>
-      ) : null}
-    </>
-  );
+  return isDialogOpen ? (
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
+      onClick={onCloseDialog}
+    >
+      <div
+        className="p-6 bg-gray-800 rounded-lg shadow-xl"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          className="px-3 py-1 bg-blue-600 rounded hover:bg-blue-700"
+          onClick={onCloseDialog}
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  ) : null;
 }
